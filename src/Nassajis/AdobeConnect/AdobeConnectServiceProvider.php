206306fcf973f9dc2d1a70d2d8a5d8c80b4c1a55
Saeed <nassaji.s@gmail.com>
2018-02-21 21:59:04 +0330

    First version.

---
+++ src/Nassajis/AdobeConnect/AdobeConnectServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Nassajis\AdobeConnect;
+
+use Illuminate\Support\ServiceProvider;
+use App;
+use AdobeConnect\Config;
+use AdobeConnect\ApiClient;
+
+class AdobeConnectServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Perform post-registration booting of services.
+     */
+    public function boot() {
+        $this->publishes([
+            __DIR__ . '/../../config/adobe-connect.php' => config_path('adobe-connect.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/adobe-connect.php', 'adobe-connect'
+        );
+
+        $this->app->singleton('Nassajis\AdobeConnect\AdobeConnectClient', function ($app) {
+            $host = config('adobe-connect.host');
+            $username = config('adobe-connect.username');
+            $password = config('adobe-connect.password');
+
+            $adobeConfig = new Config($host, $username, $password);
+            return new AdobeConnectClient($adobeConfig);
+        });
+
+        App::bind('adobe-connect', function(){
+            return App::make('Nassajis\AdobeConnect\AdobeConnectClient');
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(){
+        return ['Nassajis\AdobeConnect\AdobeConnectClient'];
+    }
+
+}
